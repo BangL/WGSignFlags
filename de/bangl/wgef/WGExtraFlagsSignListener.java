2fe5ee6eafb13c10c7dcf080978d80b42072cc3d
BangL <henno.rickowski@googlemail.com>
2012-10-17 05:56:59 +0200

    Use CustomFlag

--- de/bangl/wgef/WGExtraFlagsSignListener.java
+++ de/bangl/wgef/WGExtraFlagsSignListener.java
@@ -1,6 +1,8 @@
 package de.bangl.wgef;
 
-import com.sk89q.worldguard.protection.flags.SetFlag;
+import com.mewin.WGCustomFlags.flags.CustomSetFlag;
+import com.sk89q.worldguard.protection.flags.RegionGroup;
+import com.sk89q.worldguard.protection.flags.StringFlag;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import java.util.Set;
@@ -19,9 +21,9 @@ import org.bukkit.event.block.SignChangeEvent;
 public class WGExtraFlagsSignListener implements Listener {
     private WGExtraFlagsPlugin plugin;
 
-    public static final SetFlag<String> FLAG_SIGN_BLOCK = new SetFlag<>("sign-block", null);
-    public static final SetFlag<String> FLAG_SIGN_ALLOW = new SetFlag<>("sign-allow", null);
-    public static final SetFlag<String> FLAG_SIGN_XALLOW = new SetFlag<>("sign-xallow", null);
+    public static final CustomSetFlag FLAG_SIGNS_BLOCK = new CustomSetFlag("signs-block", new StringFlag("sign-block", RegionGroup.ALL));
+    public static final CustomSetFlag FLAG_SIGNS_ALLOW = new CustomSetFlag("signs-allow", new StringFlag("sign-allow", RegionGroup.ALL));
+    public static final CustomSetFlag FLAG_SIGNS_XALLOW = new CustomSetFlag("signs-xallow", new StringFlag("sign-xallow", RegionGroup.ALL));
 
     public WGExtraFlagsSignListener(WGExtraFlagsPlugin plugin) {
         this.plugin = plugin;
@@ -30,38 +32,14 @@ public class WGExtraFlagsSignListener implements Listener {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
 
         // Register custom flags
-        plugin.getWGCFP().addCustomFlag(FLAG_SIGN_BLOCK);
-        plugin.getWGCFP().addCustomFlag(FLAG_SIGN_ALLOW);
-        plugin.getWGCFP().addCustomFlag(FLAG_SIGN_XALLOW);
+        plugin.getWGCFP().addCustomFlag(FLAG_SIGNS_BLOCK);
+        plugin.getWGCFP().addCustomFlag(FLAG_SIGNS_ALLOW);
+        plugin.getWGCFP().addCustomFlag(FLAG_SIGNS_XALLOW);
 
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onSignChangeXXH(final SignChangeEvent event) {
-        onSignChangeEvent(event, EventPriority.MONITOR);
-    }
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onSignChangehXH(final SignChangeEvent event) {
-        onSignChangeEvent(event, EventPriority.HIGHEST);
-    }
-    @EventHandler(priority = EventPriority.HIGH)
-    public void onSignChangeH(final SignChangeEvent event) {
-        onSignChangeEvent(event, EventPriority.HIGH);
-    }
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onSignChangeM(final SignChangeEvent event) {
-        onSignChangeEvent(event, EventPriority.NORMAL);
-    }
-    @EventHandler(priority = EventPriority.LOW)
-    public void onSignChangeL(final SignChangeEvent event) {
-        onSignChangeEvent(event, EventPriority.LOW);
-    }
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onSignChangeXL(final SignChangeEvent event) {
-        onSignChangeEvent(event, EventPriority.LOWEST);
-    }
-
-    public void onSignChangeEvent(final SignChangeEvent event, EventPriority prio) {
+    public void onSignChangeEvent(final SignChangeEvent event) {
 
         Player player = event.getPlayer();
         RegionManager regionManager = plugin.getWGP().getRegionManager(player.getWorld());
@@ -69,14 +47,14 @@ public class WGExtraFlagsSignListener implements Listener {
 
         for (ProtectedRegion region : regionManager.getRegions().values()) {
             // Is this sign exclusively allowed anywhere in this world?
-            if (this.checkFlag(FLAG_SIGN_XALLOW, region, event.getLine(0).toLowerCase())) {
+            if (this.checkFlag(FLAG_SIGNS_XALLOW, region, event.getLine(0).toLowerCase())) {
 
                 // There is atleast one region in this world
                 // which allows this sign exclusively,
                 // let's see if we are the lucky one in it ...
                 for (ProtectedRegion applicableRegion : 
                         regionManager.getApplicableRegions(loc)) {
-                    if (this.checkFlag(FLAG_SIGN_XALLOW, applicableRegion, event.getLine(0).toLowerCase())) {
+                    if (this.checkFlag(FLAG_SIGNS_XALLOW, applicableRegion, event.getLine(0).toLowerCase())) {
                         // We are in the lucky region! let's allow it...
                         return;
                     }
@@ -92,7 +70,7 @@ public class WGExtraFlagsSignListener implements Listener {
         // This was not an exclusively allowed sign, so let's check if we are blacklisted...
         for (ProtectedRegion applicableRegion : 
                 regionManager.getApplicableRegions(loc)) {
-            if (checkFlag(FLAG_SIGN_BLOCK, applicableRegion, event.getLine(0).toLowerCase())) {
+            if (checkFlag(FLAG_SIGNS_BLOCK, applicableRegion, event.getLine(0).toLowerCase())) {
                 // Whoops! let's cancel this... 
                 String msg = this.plugin.getConfig().getString("You are not allowed to place this kind of sign in this region.");
                 player.sendMessage(ChatColor.RED + msg);
@@ -103,7 +81,7 @@ public class WGExtraFlagsSignListener implements Listener {
 
     }
 
-    private Boolean checkFlag(SetFlag<String> flagtype, ProtectedRegion region, String signname) {
+    private Boolean checkFlag(CustomSetFlag<String> flagtype, ProtectedRegion region, String signname) {
         Set<String> items = (Set<String>)region.getFlag(flagtype);
         return items.contains(signname);
     }
