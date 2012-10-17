0eebd980c734513c021abe710ac90756ea295f63
BangL <henno.rickowski@googlemail.com>
2012-10-17 09:34:45 +0200

    Permissions

--- de/bangl/wgef/WGExtraFlagsPlugin.java
+++ de/bangl/wgef/WGExtraFlagsPlugin.java
@@ -1,7 +1,15 @@
 package de.bangl.wgef;
 
 import com.mewin.WGCustomFlags.WGCustomFlagsPlugin;
+import com.sk89q.bukkit.util.CommandsManagerRegistration;
+import com.sk89q.minecraft.util.commands.CommandsManager;
+import com.sk89q.minecraft.util.commands.SimpleInjector;
+import com.sk89q.wepif.PermissionsResolverManager;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.bukkit.commands.ProtectionCommands;
+import com.sk89q.worldguard.bukkit.commands.ToggleCommands;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -10,6 +18,13 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class WGExtraFlagsPlugin extends JavaPlugin {
 
+    /**
+     * Manager for commands. This automatically handles nested commands,
+     * permissions checking, and a number of other fancy command things.
+     * We just set it up and register commands against it.
+     */
+    private final CommandsManager<CommandSender> commands;
+
     // Plugins
     private WGCustomFlagsPlugin pluginWGCustomFlags;
     private WorldGuardPlugin pluginWorldGuard;
@@ -17,6 +32,16 @@ public class WGExtraFlagsPlugin extends JavaPlugin {
     // Listeners
     private WGExtraFlagsSignListener signListener;
 
+    public WGExtraFlagsPlugin() {
+        final WGExtraFlagsPlugin plugin = this;
+        commands = new CommandsManager<CommandSender>() {
+            @Override
+            public boolean hasPermission(CommandSender player, String perm) {
+                return plugin.getWGP().hasPermission(player, perm);
+            }
+        };
+    }
+    
     public WGCustomFlagsPlugin getWGCFP() {
         return pluginWGCustomFlags;
     }
@@ -27,6 +52,14 @@ public class WGExtraFlagsPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        // Set the proper command injector
+        commands.setInjector(new SimpleInjector(this));
+
+        // Register command classes
+        final CommandsManagerRegistration reg = new CommandsManagerRegistration(this, commands);
+        reg.register(ToggleCommands.class);
+        reg.register(ProtectionCommands.class);
 
         // Load config
         Utils.loadConfig(this);
@@ -41,6 +74,7 @@ public class WGExtraFlagsPlugin extends JavaPlugin {
 
         // Register all listeners
         this.signListener = new WGExtraFlagsSignListener(this);
+        
     }
 
     @Override
@@ -50,4 +84,5 @@ public class WGExtraFlagsPlugin extends JavaPlugin {
         this.pluginWorldGuard = null;
         this.signListener = null;
     }
+
 }
